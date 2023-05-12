fb46248ddbc814135e42243291e8149221d07620
Barnoch Anas <anasbarnoch@gmail.com>
2023-05-12 14:42:11 +0100

    Connection with database

--- src/main/java/com/libraryonline/LibraryOnline/LibraryOnlineApplication.java
+++ src/main/java/com/libraryonline/LibraryOnline/LibraryOnlineApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.libraryonline.LibraryOnline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class LibraryOnlineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(LibraryOnlineApplication.class, args);
 	}
 
 }
--- src/test/java/com/libraryonline/LibraryOnline/LibraryOnlineApplicationTests.java
+++ src/test/java/com/libraryonline/LibraryOnline/LibraryOnlineApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.libraryonline.LibraryOnline;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class LibraryOnlineApplicationTests {
 
 	@Test
 	void contextLoads() {
