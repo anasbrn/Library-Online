acb8a2000482c22747a2418c4d4821dada2ddbdd
Barnoch Anas <anasbarnoch@gmail.com>
2023-05-24 15:40:57 +0100

    Add categories and books page

--- src/main/java/com/libraryonline/LibraryOnline/controller/BookController.java
+++ src/main/java/com/libraryonline/LibraryOnline/controller/BookController.java
@@ -1,10 +1,15 @@
 package com.libraryonline.LibraryOnline.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/books")
+@Controller
 public class BookController {
+    @GetMapping("/books")
+    public String booksPage(){
+        return "books";
+    }
 }
--- src/main/java/com/libraryonline/LibraryOnline/controller/CategoryController.java
+++ src/main/java/com/libraryonline/LibraryOnline/controller/CategoryController.java
@@ -1,10 +1,13 @@
 package com.libraryonline.LibraryOnline.controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/categories")
+@Controller
 public class CategoryController {
+    public String categoriesPage() {
+        return "categories";
+    }
 }
