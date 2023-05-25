92e5de8538e3bf9a59e7973e63155eb841e263f9
Barnoch Anas <anasbarnoch@gmail.com>
2023-05-25 10:01:18 +0100

    List of books frontend

--- src/main/java/com/libraryonline/LibraryOnline/controller/CategoryController.java
+++ src/main/java/com/libraryonline/LibraryOnline/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.libraryonline.LibraryOnline.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Controller
 public class CategoryController {
+    @GetMapping("/categories")
     public String categoriesPage() {
         return "categories";
     }
