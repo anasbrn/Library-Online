cf8f1d07ff7b7ca7b4b079682a626897eeee70bd
Barnoch Anas <anasbarnoch@gmail.com>
2023-05-27 23:59:48 +0100

    get all books for admin

--- src/main/java/com/libraryonline/LibraryOnline/controller/BookController.java
+++ src/main/java/com/libraryonline/LibraryOnline/controller/BookController.java
@@ -1,13 +1,9 @@
 package com.libraryonline.LibraryOnline.controller;
-import com.libraryonline.LibraryOnline.model.entity.Book;
-import com.libraryonline.LibraryOnline.model.entity.Category;
 import com.libraryonline.LibraryOnline.response.BookResponse;
 import com.libraryonline.LibraryOnline.service.BookService;
-import com.libraryonline.LibraryOnline.service.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
--- src/main/java/com/libraryonline/LibraryOnline/controller/HomeController.java
+++ src/main/java/com/libraryonline/LibraryOnline/controller/HomeController.java
@@ -1,11 +1,21 @@
 package com.libraryonline.LibraryOnline.controller;
 
+import com.libraryonline.LibraryOnline.response.BookResponse;
+import com.libraryonline.LibraryOnline.service.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
+    private final BookService bookService;
+
+    public HomeController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     @GetMapping("/home")
     public String homePage() {
         return "pages/home";
@@ -13,6 +23,12 @@ public class HomeController {
 
     @GetMapping("/admin/dashboard")
     public String dashboardPage() {
-        return "pages/landing_page";
+        return "pages/dashboard/landing_page";
+    }
+    @GetMapping("/admin/books")
+    public String booksPage(Model model) {
+        List<BookResponse> books = bookService.getAllBooks();
+        model.addAttribute("books", books);
+        return "pages/dashboard/books";
     }
 }
