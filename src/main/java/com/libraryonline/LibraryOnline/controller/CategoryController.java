f37327faf87ac7cb07ca10fc1af545e5282d0972
Barnoch Anas <anasbarnoch@gmail.com>
2023-05-30 00:29:49 +0100

    edit category

--- src/main/java/com/libraryonline/LibraryOnline/controller/CategoryController.java
+++ src/main/java/com/libraryonline/LibraryOnline/controller/CategoryController.java
@@ -1,12 +1,14 @@
 package com.libraryonline.LibraryOnline.controller;
 import com.libraryonline.LibraryOnline.model.entity.Category;
 import com.libraryonline.LibraryOnline.service.CategoryService;
+import javassist.NotFoundException;
 import org.hibernate.boot.jaxb.Origin;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @CrossOrigin(origins = "*")
@@ -38,6 +40,21 @@ public class CategoryController {
     @PostMapping("/admin/category/delete")
     public String deleteBook(@RequestParam("id") Integer id) {
         this.categoryService.deleteCategory(id);
+        return "redirect:/admin/categories";
+    }
+
+    @GetMapping("/admin/category/{id}/edit")
+    public String editCategoryPage(@RequestParam("id") Integer id, Model model) throws NotFoundException {
+        Optional<Category> categoryOptional = categoryService.findById(id);
+        Category category = categoryOptional.orElseThrow(() -> new NotFoundException("Category not found with ID: " + id));
+        model.addAttribute("category", category);
+
+        return "/pages/dashboard/categories/edit";
+    }
+
+    @PostMapping("/admin/category/update")
+    public String updateCategory(@ModelAttribute("category") Category category) {
+        categoryService.update(category);
         return "redirect:/admin/categories";
     }
 
--- src/main/java/com/libraryonline/LibraryOnline/service/CategoryService.java
+++ src/main/java/com/libraryonline/LibraryOnline/service/CategoryService.java
@@ -1,11 +1,10 @@
 package com.libraryonline.LibraryOnline.service;
-import com.libraryonline.LibraryOnline.model.entity.Book;
 import com.libraryonline.LibraryOnline.model.entity.Category;
-import com.libraryonline.LibraryOnline.repository.BookRepo;
 import com.libraryonline.LibraryOnline.repository.CategoryRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -25,5 +24,13 @@ public class CategoryService {
 
     public void deleteCategory(Integer id) {
         this.categoryRepo.deleteById(id);
+    }
+
+    public Optional<Category> findById(Integer id) {
+        return this.categoryRepo.findById(id);
+    }
+
+    public Category update(Category category) {
+        return this.categoryRepo.save(category);
     }
 }
